4f04d2e79549ea2c228567b4cccfd232d48bafcd
Al Jarreau Arman Maulana <shyzago@gmail.com>
2024-09-07 15:04:04 +0700

    Aw

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ class Major:
                         id = tg_auth['user']['id']
                         first_name = tg_auth['user']['first_name'] or self.faker.first_name()
                         accounts.append({'first_name': first_name, 'id': id, 'token': token})
-                except (aiohttp.ClientError, aiohttp.ContentTypeError, KeyError) as e:
+                except (aiohttp.ClientResponseError, aiohttp.ContentTypeError, KeyError) as e:
                     self.print_timestamp(
                         f"{Fore.YELLOW + Style.BRIGHT}[ Failed To Process {query} ]{Style.RESET_ALL}"
                         f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
@@ -100,12 +100,12 @@ class Major:
                             f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
                             f"{Fore.MAGENTA + Style.BRIGHT}[ Daily Visit Already Claimed ]{Style.RESET_ALL}"
                         )
-            except aiohttp.ClientError:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Daily Visit: {str(e)} ]{Style.RESET_ALL}")
-            except Exception:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Daily Visit: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Daily Visit: {str(e.status)} ]{Style.RESET_ALL}")
+            except Exception as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Daily Visit: {str(e)} ]{Style.RESET_ALL}")
 
-    async def streak(self, token: str, first_name: str):
+    async def streak(self, token: str):
         url = 'https://major.glados.app/api/user-visits/streak/'
         headers = {
             **self.headers,
@@ -116,12 +116,12 @@ class Major:
                 async with session.get(url=url, headers=headers, timeout=20) as response:
                     response.raise_for_status()
                     return await response.json()
-            except aiohttp.ClientError:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching Streak: {str(e)} ]{Style.RESET_ALL}")
-            except Exception:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching Streak: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching Streak: {str(e.status)} ]{Style.RESET_ALL}")
+            except Exception as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching Streak: {str(e)} ]{Style.RESET_ALL}")
 
-    async def user(self, token: str, id: str, first_name: str):
+    async def user(self, token: str, id: str):
         url = f'https://major.glados.app/api/users/{id}/'
         headers = {
             **self.headers,
@@ -132,10 +132,10 @@ class Major:
                 async with session.get(url=url, headers=headers, timeout=20) as response:
                     response.raise_for_status()
                     return await response.json()
-            except aiohttp.ClientError as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching User: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
 
     async def squad(self, token: str, squad_id: int, first_name: str):
         url = f'https://major.glados.app/api/squads/{squad_id}'
@@ -153,12 +153,12 @@ class Major:
                         f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
                         f"{Fore.GREEN + Style.BRIGHT}[ Squad {squad['name']} ]{Style.RESET_ALL}"
                         f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                        f"{Fore.GREEN + Style.BRIGHT}[ Squad Rating {squad['rating']} ]{Style.RESET_ALL}"
+                        f"{Fore.BLUE + Style.BRIGHT}[ Squad Rating {squad['rating']} ]{Style.RESET_ALL}"
                     )
-            except aiohttp.ClientError as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching Squad: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching Squad: {str(e)} ]{Style.RESET_ALL}")
 
     async def join_squad(self, token: str, first_name: str):
         url = f'https://major.glados.app/api/squads/1904705154/join/'
@@ -175,10 +175,10 @@ class Major:
                     join_squad = await response.json()
                     if join_squad['status'] == 'ok':
                         await self.squad(token=token, squad_id=1904705154, first_name=first_name)
-            except aiohttp.ClientError as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Join Squad: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Join Squad: {str(e)} ]{Style.RESET_ALL}")
 
     async def leave_squad(self, token: str, first_name: str):
         url = f'https://major.glados.app/api/squads/leave/'
@@ -195,12 +195,12 @@ class Major:
                     leave_squad = await response.json()
                     if leave_squad['status'] == 'ok':
                         await self.join_squad(token=token, first_name=first_name)
-            except aiohttp.ClientError as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Leave Squad: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching User: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Leave Squad: {str(e)} ]{Style.RESET_ALL}")
 
-    async def tasks(self, token: str, type: str, first_name: str):
+    async def tasks(self, token: str, type: str):
         url = f'https://major.glados.app/api/tasks/?is_daily={type}'
         headers = {
             **self.headers,
@@ -211,10 +211,10 @@ class Major:
                 async with session.get(url=url, headers=headers, timeout=20) as response:
                     response.raise_for_status()
                     return await response.json()
-            except aiohttp.ClientError as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Fetching Tasks: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Fetching Tasks: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Fetching Tasks: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Fetching Tasks: {str(e)} ]{Style.RESET_ALL}")
 
     async def complete_task(self, token: str, first_name: str, task_id: int, task_title: str, task_award: int):
         url = 'https://major.glados.app/api/tasks/'
@@ -237,12 +237,10 @@ class Major:
                             f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
                             f"{Fore.GREEN + Style.BRIGHT}[ {task_title} Claimed, You've Got {task_award} ]{Style.RESET_ALL}"
                         )
-            except aiohttp.ClientError as e:
-                if e.status == 400:
-                    return
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Complete {task_title}: {str(e)} ]{Style.RESET_ALL}")
+            except aiohttp.ClientResponseError as e:
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An HTTP Error Occurred While Complete {task_title}: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Complete {task_title}: {str(e)} ]{Style.RESET_ALL}")
+                self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ An Unexpected Error Occurred While Complete {task_title}: {str(e)} ]{Style.RESET_ALL}")
 
     async def coins(self, token: str, first_name: str, reward_coins: int):
         url = 'https://major.glados.app/api/bonuses/coins/'
@@ -280,9 +278,7 @@ class Major:
                             f"{Fore.GREEN + Style.BRIGHT}[ {first_name} Got {reward_coins} From Hold Coin ]{Style.RESET_ALL}"
                         )
             except aiohttp.ClientResponseError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Play Hold Coins: {str(e)} ]{Style.RESET_ALL}")
-            except aiohttp.ClientError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} A Client Error Occurred While Play Hold Coins: {str(e)} ]{Style.RESET_ALL}")
+                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Play Hold Coins: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
                 return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Play Hold Coins: {str(e)} ]{Style.RESET_ALL}")
 
@@ -319,11 +315,9 @@ class Major:
                         f"{Fore.GREEN + Style.BRIGHT}[ Got {roulette['rating_award']} From Roulette ]{Style.RESET_ALL}"
                     )
             except aiohttp.ClientResponseError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
-            except aiohttp.ClientError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} A Client Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
+                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Play Roulette: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
+                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Play Rouelette: {str(e)} ]{Style.RESET_ALL}")
 
     async def swipe_coin(self, token: str, first_name: str, reward_swipe_coins: int):
         url = 'https://major.glados.app/api/swipe_coin/'
@@ -361,11 +355,9 @@ class Major:
                             f"{Fore.GREEN + Style.BRIGHT}[ Got {reward_swipe_coins} From Swipe Coin ]{Style.RESET_ALL}"
                         )
             except aiohttp.ClientResponseError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
-            except aiohttp.ClientError as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} A Client Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
+                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An HTTP Error Occurred While Play Swipe Coin: {str(e.status)} ]{Style.RESET_ALL}")
             except Exception as e:
-                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Bonuses Coins: {str(e)} ]{Style.RESET_ALL}")
+                return self.print_timestamp(f"{Fore.RED + Style.BRIGHT}[ {first_name} An Unexpected Error Occurred While Play Swipe Coin: {str(e)} ]{Style.RESET_ALL}")
 
     async def main(self):
         while True:
@@ -376,8 +368,8 @@ class Major:
                 for account in accounts:
                     self.print_timestamp(f"{Fore.WHITE + Style.BRIGHT}[ {account['first_name']} Information ]{Style.RESET_ALL}")
                     await self.visit(token=account['token'], first_name=account['first_name'])
-                    streak = await self.streak(token=account['token'], first_name=account['first_name'])
-                    user = await self.user(token=account['token'], id=account['id'], first_name=account['first_name'])
+                    streak = await self.streak(token=account['token'])
+                    user = await self.user(token=account['token'], id=account['id'])
                     self.print_timestamp(
                         f"{Fore.CYAN + Style.BRIGHT}[ {account['first_name']} ]{Style.RESET_ALL}"
                         f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
@@ -394,7 +386,7 @@ class Major:
                 for account in accounts:
                     self.print_timestamp(f"{Fore.WHITE + Style.BRIGHT}[ {account['first_name']} Tasks ]{Style.RESET_ALL}")
                     for type in ['true', 'false']:
-                        tasks = await self.tasks(token=account['token'], type=type, first_name=account['first_name'])
+                        tasks = await self.tasks(token=account['token'], type=type)
                         for task in tasks:
                             if task['is_completed'] == False:
                                 await self.complete_task(token=account['token'], first_name=account['first_name'], task_id=task['id'], task_title=task['title'], task_award=task['award'])
@@ -405,7 +397,7 @@ class Major:
                     await self.roulette(token=account['token'], first_name=account['first_name'])
                     await self.swipe_coin(token=account['token'], first_name=account['first_name'], reward_swipe_coins=3200)
 
-                    user = await self.user(token=account['token'], id=account['id'], first_name=account['first_name'])
+                    user = await self.user(token=account['token'], id=account['id'])
                     if user:
                         total_rating += user['rating'] if user else 0
 
